bb7dc118e75b998d98a241e0c9354b7d6df6dd53
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-05 09:01:19 +0200

    move env variables in function, add mailx variant

--- tools/emails.py
+++ tools/emails.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import ssl
 import smtplib
 
@@ -6,19 +7,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-EMAIL_SMTP_HOST = os.environ["EMAIL_SMTP_HOST"]
-EMAIL_SMTP_PORT = os.environ.get("EMAIL_SMTP_PORT", 587)
-EMAIL_USERNAME = os.environ["EMAIL_USERNAME"]
-EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
-
 
 def send_email(to_email: str, subject: str, message: str) -> None:
     """
-    Send an email message.
+    Send an email message using environment settings.
     """
+    host = os.environ["EMAIL_SMTP_HOST"]
+    port = os.environ.get("EMAIL_SMTP_PORT", 587)
+    from_email = os.environ["EMAIL_USERNAME"]
+    password = os.environ["EMAIL_PASSWORD"]
+
     context = ssl.create_default_context()
     email_message = f"Subject: {subject}\n\n{message}"
-    with smtplib.SMTP(EMAIL_SMTP_HOST, EMAIL_SMTP_PORT) as server:
+    with smtplib.SMTP(host, port) as server:
         server.starttls(context=context)
-        server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
-        server.sendmail(EMAIL_USERNAME, to_email, email_message)
+        server.login(from_email, password)
+        server.sendmail(from_email, to_email, email_message)
+
+
+def send_mailx(to_email: str, subject: str, message: str) -> None:
+    """
+    Send an email message using mailx if available.
+    """
+    if shutil.which("mailx") is None:
+        raise RuntimeError("Cannot find mailx in path.")
+
+    ret = os.system(f"echo '{message}' | mailx -s '{subject}' {to_email}")
+    if ret != 0:
+        raise RuntimeError("mailx did not return successfully.")
