a78bb78df9a63a004c9f627363d39c8b572f4de4
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-05 10:24:32 +0200

    add default to email (if you send notes to yourself a lot) + exception handling

--- tools/email.py
+++ tools/email.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import ssl
 import smtplib
 
@@ -8,20 +7,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def send_email(to_email: str, subject: str, message: str) -> None:
+class MissingRecipientEmail(Exception):
+    """Exception to be raised if no recipient email is provided"""
+
+
+def send_email(subject: str, message: str, to_email: str = None) -> None:
     """
-    Send an email message using environment settings.
+    Send an email message. Retrieve config from environment. If to_email is not
+    provided try sending it to EMAIL_DEFAULT_TO_EMAIL.
     """
     host = os.environ["EMAIL_SMTP_HOST"]
     port = os.environ.get("EMAIL_SMTP_PORT", 587)
     from_email = os.environ["EMAIL_USERNAME"]
     password = os.environ["EMAIL_PASSWORD"]
+    to_email = to_email or os.environ.get("EMAIL_DEFAULT_TO_EMAIL")
+
+    if to_email is None:
+        raise MissingRecipientEmail("Need to specify a recipient email")
 
     context = ssl.create_default_context()
-    email_message = f"Subject: {subject}\n\n{message}"
+
     with smtplib.SMTP(host, port) as server:
         server.starttls(context=context)
         server.login(from_email, password)
+
+        email_message = f"Subject: {subject}\n\n{message}"
         server.sendmail(from_email, to_email, email_message)
 
 
@@ -31,4 +41,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--subject", required=True)
     parser.add_argument("-m", "--message", required=True)
+    parser.add_argument("-e", "--email")
+
     args = parser.parse_args()
+    send_email(args.subject, args.message, args.email)
