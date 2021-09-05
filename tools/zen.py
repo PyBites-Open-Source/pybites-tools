41f741e398e3e15f0e9c648154068636a64d3ac4
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-05 11:25:03 +0200

    add pyperclip to copy zen to clipboard

--- tools/zen.py
+++ tools/zen.py
@@ -10,7 +10,15 @@ def zen_of_python() -> list[str]:
     zen = StringIO()
     old_stdout = sys.stdout
     sys.stdout = zen
-    import this
+    import this  # noqa F401
 
     sys.stdout = old_stdout
     return zen.getvalue().splitlines()
+
+
+if __name__ == "__main__":
+    import pyperclip
+
+    zen = "\n".join(zen_of_python())
+    pyperclip.copy(zen)
+    print("The Zen of Python has been copied to your clipboard")
