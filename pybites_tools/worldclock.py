72877f1ad1775799ac6c49003b6619b8b22b0911
edg <glutenfreepy@gmail.com>
2021-10-08 11:50:19 -0700

    add excepts for catching timezone errors

--- pybites_tools/worldclock.py
+++ pybites_tools/worldclock.py
@@ -4,16 +4,25 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from dotenv import load_dotenv
+import pytz
 
 load_dotenv()
 
 
 def main():
-    timezones = json.loads(os.environ["TIMEZONE_LIST"])
-    for zone in timezones:
-        zone_now = datetime.now(ZoneInfo(zone))
-        formatted_time = zone_now.strftime("%I:%M%p")
-        print(f"{zone:25} {formatted_time}")
+    try:
+        timezones = json.loads(os.environ["TIMEZONE_LIST"])
+        for zone in timezones:
+            zone_now = datetime.now(pytz.timezone(zone))
+            formatted_time = zone_now.strftime("%I:%M%p")
+            print(f"{zone:25} {formatted_time}")
+    except pytz.exceptions.UnknownTimeZoneError:
+        print(
+            "UnknownTimeZoneError occurred.  Please check your .env file for correct timezone names"
+        )
+    except json.decoder.JSONDecodeError as d:
+        print("JSON error occurred.  Please check your .env file for syntax")
+        print(d.args)
 
 
 if __name__ == "__main__":
